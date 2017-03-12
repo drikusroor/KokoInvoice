a42bff5da60cd42fd4df8b914e539ed4bd456b25
Drikus Roor <drikusroor@gmail.com>
2017-03-12 15:46:33 +0100

    Koko style now only applied to the actual invoice, google material will take care of the admin part

--- src/app/koko-invoice/koko-invoice.component.ts
+++ src/app/koko-invoice/koko-invoice.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'invoice',
-  template: `<div>
+  template: `<div class="factuur">
     <header></header>
     <content></content>
     </div>`
